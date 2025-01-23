215759e5eaade6c7174dfe6dc0a2c663349e7262
Yerram Sai Teja <146211807+yerramsaiteja@users.noreply.github.com>
2025-01-23 11:42:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from unittest import result
+from flask import Flask, render_template, request, jsonify
+
+app=Flask(__name__)
+
+import pickle
+#model=pickle.load(open(r"C:\Users\SAI TEJA\Downloads\model.html.pkl","rb"))
+
+@app.route('/')
+def homepage():
+    # return render_template(r"index.html")
+    return "Sai "
+
+@app.route('/outputpage',methods=['POST'])
+def outputpage():
+    A1=request.form['a1']
+    A2=request.form['a2']
+    A3=request.form['a3']
+    A4=request.form['a4']
+    A5=request.form['a5']
+    A6=request.form['a6']
+    A7=request.form['a7']
+    A8=request.form['a8']
+    A9=request.form['a9']
+    A10=request.form['a10']
+    A11=request.form['a11']
+    A12=request.form['a12']
+    A13=request.form['a13']
+    A14=request.form['a14']
+    
+    test=[[float(A1),float(A2),float(A3),float(A4),float(A5),float(A6),float(A7),float(A8),float(A9),float(A10),float(A11),float(A12),float(A13),float(A14)]]
+    #result=model.predict(test)
+    if(result[0]==0):
+        res="The person doesn't have parkinson"
+    else:
+        res="The person has parkinson"
+    return render_template(r"C:\Users\SAI TEJA\Downloads\index.html",y=res)
+
+if __name__=='__main__':
+    app.run(debug=True)
